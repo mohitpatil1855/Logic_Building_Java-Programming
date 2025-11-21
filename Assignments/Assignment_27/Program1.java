c55ef3639fc204e6a7026aa63aa26935e67286ca
mohitpatil1855 <mohitpatil0185@gmail.com>
2025-11-21 23:59:19 +0530

    Add files via upload

---
+++ Assignments/Assignment_27/Program1.java
@@ -0,0 +1,46 @@
+/*
+    * * * * 
+    * * * *
+    * * * *
+ */
+
+import java.util.*;
+
+class pattern {
+
+    public void Display(int iRow, int iCol) {
+        int i = 0;
+        int j = 0;
+
+        for (i=1;i<=iRow;i++ )
+        {
+            for(j=1;j<=iCol;j++)
+            {
+                System.out.print(j+"\t");
+            }
+            System.out.println("");
+        }
+    }
+}
+
+class Program1
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        int iValue1 = 0;
+        int iValue2 = 0;
+
+        System.out.println("Enter Number of Rows: ");
+        iValue1 = sobj.nextInt();
+
+        System.out.println("Enter Number of Colomns: ");
+        iValue2 = sobj.nextInt();
+
+        pattern pobj = new pattern();
+
+        pobj.Display(iValue1,iValue2);
+        
+    }
+}
---
+++ Assignments/Assignment_27/program5.java
@@ -0,0 +1,49 @@
+/*
+Input:row  : 4
+      colomn  : 6
+
+   1 1 1 1 1 1 
+   2 2 2 2 2 2 
+   3 3 3 3 3 3 
+   
+ */
+import java.util.*;
+
+class pattern {
+
+    public void Display(int iRow, int iCol) {
+        int i = 0;
+        int j = 0;
+
+        for (i=1;i<=iRow;i++ )
+        {
+            for(j=1;j<=iCol;j++)
+            {
+                System.out.print(i+"\t");
+            }
+            System.out.println("");
+        }
+    }
+}
+
+class program5
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        int iValue1 = 0;
+        int iValue2 = 0;
+
+        System.out.println("Enter Number of Rows: ");
+        iValue1 = sobj.nextInt();
+
+        System.out.println("Enter Number of Colomns: ");
+        iValue2 = sobj.nextInt();
+
+        pattern pobj = new pattern();
+
+        pobj.Display(iValue1,iValue2);
+        
+    }
+}
