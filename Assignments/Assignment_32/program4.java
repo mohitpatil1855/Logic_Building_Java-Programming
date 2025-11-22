19ee5e1908959b8eb4d8ee2735d2718b98fd83de
mohitpatil1855 <mohitpatil0185@gmail.com>
2025-11-22 11:41:14 +0530

    Add files via upload

---
+++ Assignments/Assignment_32/program4.java
@@ -0,0 +1,63 @@
+
+/* 
+*       *       *       *       *       *
+*       #       #       #       *       *
+*       #       #       *       $       *
+*       #       *       $       $       *
+*       *       $       $       $       *
+*       *       *       *       *       *
+ */
+import java.util.*;
+
+class pattern {
+
+    public void Display(int iRow, int iCol) {
+        int i = 0;
+        int j = 0;
+
+        //Filter
+        if (iRow != iCol) {
+            System.out.println("Invalid Input..!");
+            System.out.println("Ender Number of Rows and Colomns Same..");
+            return;
+        }
+
+        for (i = 1; i <= iRow; i++) {
+
+            for (j = 1; j <= iCol; j++) {
+                if (i == 1 || i == iRow || j == 1 || j == iCol || (iCol + 1) == (i + j)) {
+                    System.out.print("*\t");
+                } else if ((iCol + 1) > (i + j)) {
+                    System.out.print("#\t");
+                } else {
+                    System.out.print("$\t");
+                }
+
+            }
+
+            System.out.println();
+
+        }
+    }
+}
+
+class program4 {
+
+    public static void main(String A[]) {
+        Scanner sobj = new Scanner(System.in);
+
+        int iValue1 = 0;
+        int iValue2 = 0;
+
+        System.out.println("Enter Number of Rows: ");
+        iValue1 = sobj.nextInt();
+
+        System.out.println("Enter Number of Colomns: ");
+        iValue2 = sobj.nextInt();
+
+        pattern pobj = new pattern();
+
+        pobj.Display(iValue1, iValue2);
+
+    }
+}
